0307ec0e15e7e51098cc084e3118c3720cc635de
Assister2 <staniwitkow@gmail.com>
2023-10-26 03:05:14 -0700

    feat: add signin and signup

--- popup/popup.js
+++ popup/popup.js
@@ -17,11 +17,40 @@ document.getElementById("loginForm").addEventListener("submit", function () {
     if (password.length < 8) {
         document.getElementById("loginPassword").style.borderColor = "red";
     }
-    alert("here");
-    alert(email && validateEmail(email) && password.length >= 8);
+    
     if (email && validateEmail(email) && password.length >= 8) {
-        chrome.runtime.sendMessage({message: 'login', payload: {email, password}}, function (response) {
-            alert(response);
+        chrome.runtime.sendMessage({message: 'login', payload: {email, password}}, response => {
+            console.log(response);
+            if (response) {
+                chrome.storage.local.set({
+                    'email': email,
+                    'access_token': response.access_token,
+                    'refresh_token': response.refresh_token,
+                })
+            }
+        })
+    }
+});
+
+document.getElementById("signupForm").addEventListener("submit", function () {
+    let email = document.getElementById("signupEmail").value;
+    let password = document.getElementById("signupPassword").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (!validateEmail(email)) {
+        document.getElementById("signupEmail").style.borderColor = "red";
+    }
+    if (password.length < 8) {
+        document.getElementById("signupPassword").style.borderColor = "red";
+    }
+
+    if (confirmPassword.length < 8) {
+        document.getElementById("confirmPassword").style.borderColor = "red";
+    }
+
+    if (email && validateEmail(email) && password.length >= 8 && password === confirmPassword) {
+        chrome.runtime.sendMessage({message: 'signup', payload: {email, password}}, response => {
+            console.log(response);
         })
     }
 });
--- service_worker.js
+++ service_worker.js
@@ -1,14 +1,36 @@
 chrome.runtime.onMessage.addListener(function (message, sender, senderResponse) {
+    console.log(message.payload);
     if (message.message === "login") {
         fetch('https://api.croboost.ai/v1/auth/manual/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email: sender.email, password: sender.password})
+            body: JSON.stringify({email: message.payload.email, password: message.payload.password})
         }).then(res => {
             return res.json();
         }).then(res => {
+            console.log(res);
+            senderResponse(res);
+        })
+    }
+
+    if (message.message === "signup") {
+        fetch('https://api.croboost.ai/v1/auth/manual/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: message.payload.email,
+                confirm_email: message.payload.email,
+                password: message.payload.password,
+                confirm_password: message.payload.password
+            })
+        }).then(res => {
+            return res.json();
+        }).then(res => {
+            console.log(res);
             senderResponse(res);
         })
     }
