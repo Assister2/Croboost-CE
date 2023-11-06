c02ba9bb8059ec3de540b06e431e6865dd039665
Assister2 <staniwitkow@gmail.com>
2023-11-06 14:28:34 -0700

    fix: converting to jquery

--- js/service_worker.js
+++ js/service_worker.js
@@ -47,8 +47,37 @@ chrome.runtime.onMessage.addListener(function (message, sender, senderResponse) 
         fetch('https://api.croboost.ai/v1/metrics/'+message.payload.record_id, {
             method: 'GET',
             headers: {
-                'Authorization': "Bearer "+ message.payload.access_token.access_token
+                'Authorization': "Bearer "+ message.payload.temp.access_token
             },
+        }).then(res => {
+            return res.json();
+        }).then(res => {
+            senderResponse(res);
+        })
+    }
+
+    if (message.message === "createTest") {
+        console.log(message.payload)
+        fetch('https://api.croboost.ai/v1/ab/tests/'+message.payload.record_id, {
+            method: 'POST',
+            headers: {
+                'Authorization': "Bearer "+ message.payload.token.access_token
+            },
+            body: message.payload.data
+        }).then(res => {
+            return res.json();
+        }).then(res => {
+            senderResponse(res);
+        })
+    }
+
+    if (message.message === "getAi") {
+        fetch('http://localhost:3001/'+message.payload.aiOption, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: message.payload.data
         }).then(res => {
             return res.json();
         }).then(res => {
